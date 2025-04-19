c8ef5796be5c8db31606bff224ee89eae7bb3f1c
Luis Villa <luisji2312ia@gmail.com>
2025-04-19 02:59:00 -0600

    Definicion de Rutas en Router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,8 @@
+import 'package:easycoutcol/config/presentation/screens/config_screen.dart';
+import 'package:easycoutcol/config/presentation/screens/history_screen.dart';
 import 'package:easycoutcol/config/presentation/screens/home_screen.dart';
+import 'package:easycoutcol/config/presentation/screens/login_screen.dart';
+import 'package:easycoutcol/config/presentation/screens/results_screen.dart';
 import 'package:easycoutcol/config/presentation/screens/tutorial_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,7 +17,28 @@ final appRouter = GoRouter(
     ),
     GoRoute(
       path: '/tutorial',
+      name: TutorialScreen.name,
       builder: (context, state) => TutorialScreen(),
+    ),
+    GoRoute(
+      path: '/login',
+      name: LoginScreen.name,
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
+      path: '/history',
+      name: HistoryScreen.name,
+      builder: (context, state) => HistoryScreen(),
+    ),
+    GoRoute(
+      path: '/results',
+      name: ResultsScreen.name,
+      builder: (context, state) => ResultsScreen(),
+    ),
+    GoRoute(
+      path: '/config',
+      name: ConfigScreen.name,
+      builder: (context, state) => ConfigScreen(),
     ),
   ],
 );
