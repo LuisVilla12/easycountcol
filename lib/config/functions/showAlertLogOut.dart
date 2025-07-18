1f676da85d289b179a7374920ca555a02b99d9fb
LuisVilla12 <luisji2312ia@gmail.com>
2025-07-17 23:57:13 -0600

    Docker inicio

--- lib/config/functions/showAlertLogOut.dart
+++ lib/config/functions/showAlertLogOut.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 void showAlertLogOut(BuildContext context) {
   showDialog(
     context: context,
@@ -16,7 +12,8 @@ void showAlertLogOut(BuildContext context) {
       content: const Text('¿Estás seguro de que deseas cerrar sesión?'),
       actions: [
         TextButton(
-          onPressed: () => Navigator.of(context).pop(), // Cierra solo el diálogo
+          onPressed: () =>
+              Navigator.of(context).pop(), // Cierra solo el diálogo
           child: const Text('Cancelar'),
         ),
         ElevatedButton(
